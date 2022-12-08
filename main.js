d85793a3e9207d52cbbd816f7923e3390e3fded1
dinine <letbegin201@gmail.com>
2022-12-09 00:04:46 +0700

    f

---
+++ main.js
@@ -0,0 +1,13 @@
+var languages = ['Dung', 21, 'tuan', 'dao']
+var languages2 = ['alright', 77, 'now', 'fun']
+
+
+// languages.splice(1, 0)
+
+// console.log(languages.slice(-2, -1))
+
+function showInfo(chat) {
+    alert(chat)
+}
+
+
